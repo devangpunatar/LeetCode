b22074d3a8d9c5e8fa11c25f306403b49c6308e7
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2024-07-30 18:58:03 -0700

    Time: 42 ms (18.51%), Space: 16.4 MB (90.94%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,11 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            # If the digit is less than 9, just add one to this digit and return the list
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            # If the digit is 9, it becomes 0 and we continue to the next digit
+            digits[i] = 0
         
-        num = 0
-        for d in digits:
-            num = (num * 10) + d
-        num = num + 1
-        
-        digits = [int(d) for d in str(num)]
-        
-        return digits
+        # If we have gone through all digits and all were 9, we need to add an additional 1 at the beginning
+        return [1] + digits
