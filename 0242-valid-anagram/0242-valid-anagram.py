4c27a4a6a134b4fd8876e2b099e78546df4415f4
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2024-11-19 06:49:29 -0800

    Time: 12 ms (59.40%), Space: 16.7 MB (73.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
         if len(s) != len(t):
             return False
-
+        
         count = {}
-
-        # Count characters in `s`
-        for char in s:
-            count[char] = count.get(char, 0) + 1
-
-        # Decrement counts based on characters in `t`
-        for char in t:
-            if char not in count:
+        
+        for c in s:
+            count[c] = count.get(c, 0) + 1
+            
+        for c in t:
+            if c not in count:
                 return False
-            count[char] -= 1
-            if count[char] < 0:
+            count[c] -= 1
+            if count[c] < 0:
                 return False
-
-        # If all counts are zero, they are anagrams
+            
         return True
+                
