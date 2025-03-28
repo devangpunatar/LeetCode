79395f157783cdb5d88d8908c1b192d6b2956993
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-03-28 16:00:25 -0700

    File updated at 3/28/2025, 4:00:25 PM

--- 680-ValidPalindromeII/680-ValidPalindromeII.py
+++ 680-ValidPalindromeII/680-ValidPalindromeII.py
@@ -1,14 +1,14 @@
-# Last updated: 3/28/2025, 3:57:10 PM
+# Last updated: 3/28/2025, 4:00:25 PM
 class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        
-        def check(s, l, r):
-            return s[l:r+1] == s[l:r+1][::-1]
-
+    def validPalindrome(self, s: str, strike=False) -> bool:
+    
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                return check(s, l + 1, r) or check(s, l, r - 1)
-            l += 1
-            r -= 1
+                if strike:
+                    return False
+                return self.validPalindrome(s[l:r], True) or self.validPalindrome(s[l + 1:r + 1], True)
+            else:
+                l += 1
+                r -= 1
         return True
