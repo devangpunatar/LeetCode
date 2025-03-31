a7827ded94243828fe6e595c49fadd10bb0578f1
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-03-31 05:27:32 -0700

    Time: 625 ms (57.06%) | Memory: 25.4 MB (48.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,43 +1,24 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        
-        def merge_sort(nums):
-            n = len(nums)
+        if len(nums) <= 1:
+            return nums
 
-            if n == 1:
-                return nums
-            m = n // 2
-            L = nums[:m]
-            R = nums[m:] 
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
 
-            L = merge_sort(L)
-            R = merge_sort(R)
-            l, r = 0, 0
-            L_len = len(L)
-            R_len = len(R)
+        l, r = 0, 0
+        merged = []
 
-            sorted_arr = [0] * n
-            i = 0
-
-            while l < L_len and r < R_len:
-                if L[l] <= R[r]:
-                    sorted_arr[i] = L[l]
-                    l += 1
-                else:
-                    sorted_arr[i] = R[r]
-                    r += 1
-                i += 1
-            
-            while l < L_len:
-                sorted_arr[i] = L[l]
+        while l < len(left) and r < len(right):
+            if left[l] <= right[r]:
+                merged.append(left[l])
                 l += 1
-                i += 1
-            
-            while r < R_len:
-                sorted_arr[i] = R[r]
-                i += 1
+            else:
+                merged.append(right[r])
                 r += 1
 
-            return sorted_arr
+        merged.extend(left[l:])
+        merged.extend(right[r:])
 
-        return merge_sort(nums)
+        return merged
