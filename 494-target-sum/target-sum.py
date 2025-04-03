f706e871741176e0fda2b07e23ada36c338e4519
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-02 17:48:31 -0700

    Time: 124 ms (64.27%) | Memory: 18.2 MB (58.58%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,5 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+
+        dp[0][0] = 1 # (0 elements, 0 sum) -> 1 way
+
+        for i in range(len(nums)):
+            for cur_sum, count in dp[i].items():
+                dp[i + 1][cur_sum + nums[i]] += count
+                dp[i + 1][cur_sum - nums[i]] += count
+
+        return dp[len(nums)][target]
+
+        '''
+        memoization
+
         dp = {} # (index, cur_sum) -> number of ways
 
         def backtrack(i, cur_sum):
@@ -14,5 +29,6 @@ class Solution:
                 backtrack(i + 1, cur_sum - nums[i])
                 )
             return dp[(i, cur_sum)]
-            
+
         return backtrack(0, 0)
+        '''
