ee03b5781b516e656c11181a2ebc9d771d961ea4
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-15 00:42:20 -0700

    Time: 5 ms (38.22%) | Memory: 19.8 MB (20.02%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visited, cycle = set(), set()
+        output = []
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visited:
+                return True
+            cycle.add(crs)
+
+            for pre in preMap[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visited.add(crs)
+            output.append(crs)
+
+            return True
+
+        for crs in range(numCourses):
+            if dfs(crs) == False:
+                return []
+        return output
+
+
+
+
+
+    
+    
