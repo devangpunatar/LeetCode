fbdb6f7a126f56ad397a325e04b6b452e3a89351
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-16 00:38:51 -0700

    Time: 4 ms (2.19%) | Memory: 17.9 MB (11.59%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,11 +1,6 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         
-        hashMap = {}
-        res = 0
-        for i in jewels:
-            hashMap[i] = hashMap.get(i, 0)
-        for c in stones:
-            if c in hashMap:
-                res += 1
-        return res
+        JewelSet = set(jewels)
+        return sum(stone in JewelSet for stone in stones)
+        
