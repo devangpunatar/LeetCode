c93c21c12753124995c470dfc31f0363f96425d9
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-19 16:30:05 -0700

    Time: 45 ms (39.87%) | Memory: 17.9 MB (24.34%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,45 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # Recursive Solution
+        def multiply(F, M):
+            x = F[0][0] * M[0][0] + F[0][1] * M[1][0]
+            y = F[0][0] * M[0][1] + F[0][1] * M[1][1]
+            z = F[1][0] * M[0][0] + F[1][1] * M[1][0]
+            w = F[1][0] * M[0][1] + F[1][1] * M[1][1]
+            F[0][0], F[0][1], F[1][0], F[1][1] = x, y, z, w
 
-        # if n == 0 or n == 1:
-        #     return n
+        def power(F, n):
+            if n == 0 or n == 1:
+                return
+            M = [[1, 1], [1, 0]]
+            power(F, n // 2)
+            multiply(F, F)
+            if n % 2 != 0:
+                multiply(F, M)
 
-        # return self.fib(n - 1) + self.fib(n - 2)
-
-        # Top Down DP (Memoization)
-
-        # memo = {0: 0, 1: 1}
-
-        # def f(x):
-        #     if x in memo:
-        #         return memo[x]
-        #     else:
-        #         memo[x] = f(x - 1) + f(x - 2)
-        #         return memo[x]
-
-        # return f(n)
-
-        # Bottom Up DP (Tabulation)
-        
-        # if n < 2:
-        #     return n
-
-        # dp = [0] * (n + 1)
-        # dp[1] = 1
-
-        # for i in range(2, n + 1):
-        #     dp[i] = dp[i - 1] + dp[i - 2]
-
-        # return dp[n]
-
-        # Bottom Up DP (O(1) Space)
-
-        if n < 2:
-            return n
-        
-        prev, curr = 0, 1
-        for i in range(2, n + 1):
-            prev, curr = curr, prev + curr
-
-        return curr
+        if n == 0:
+            return 0
+        F = [[1, 1], [1, 0]]
+        power(F, n - 1)
+        return F[0][0]
