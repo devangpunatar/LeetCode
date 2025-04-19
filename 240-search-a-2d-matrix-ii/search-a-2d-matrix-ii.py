c7c2685fad0bec166ea7f974226c5da62d8a34e6
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-19 03:00:23 -0700

    Time: 146 ms (49.99%) | Memory: 23.9 MB (96.48%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,17 +1,12 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
         m, n = len(matrix), len(matrix[0])
-        hashset_i = set()
-        hashset_j = set()
+
         for i in range(m):
             for j in range(n - 1, -1, -1):
                 if target == matrix[i][j]:
                     return True
                 elif target > matrix[i][j]:
                     break
-                elif target < matrix[i][j]:
-                    continue
-        
         return False
 
