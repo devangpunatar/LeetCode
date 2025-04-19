f04b1e217764191246b9f2a650544218877d5345
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-19 16:27:04 -0700

    Time: 40 ms (59.33%) | Memory: 17.6 MB (89.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -20,13 +20,26 @@ class Solution:
 
         # return f(n)
 
-        # Bottom Up Dp (Tabulation)
+        # Bottom Up DP (Tabulation)
+        
+        # if n < 2:
+        #     return n
+
+        # dp = [0] * (n + 1)
+        # dp[1] = 1
+
+        # for i in range(2, n + 1):
+        #     dp[i] = dp[i - 1] + dp[i - 2]
+
+        # return dp[n]
+
+        # Bottom Up DP (O(1) Space)
+
         if n < 2:
             return n
-        dp = [0] * (n + 1)
-        dp[1] = 1
-
+        
+        prev, curr = 0, 1
         for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+            prev, curr = curr, prev + curr
 
-        return dp[n]
+        return curr
