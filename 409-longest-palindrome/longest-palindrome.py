96b5982fd0e3178154a5fc3f5dd642f3177497fc
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-20 03:18:02 -0700

    Time: 3 ms (42.24%) | Memory: 17.8 MB (46.85%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        
+        '''
         count = {}
         res = 0
         for char in s:
@@ -15,6 +15,24 @@ class Solution:
                 count[char] -= temp
                 res += temp
         return res + 1 if sum(count.values()) > 0 else res
+
+
+        '''
+        # Neetcode's solution
+
+        count = collections.defaultdict(int)
+        res = 0
+        for char in s:
+            count[char] += 1
+            if count[char] % 2 == 0:
+                res += 2
+        
+        for cnt in count.values():
+            if cnt % 2 != 0:
+                res += 1
+                break
+        
+        return res
 
 
                 
