623ad733ad4f7d9b76fd11239df9528527eacd31
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-20 00:38:12 -0700

    Time: 3026 ms (19.01%) | Memory: 17.7 MB (84.68%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        def check(i, j):
+        def checkPalindrome(i, j):
             left = i
             right = j - 1
 
@@ -10,10 +10,12 @@ class Solution:
                     return False
                 left += 1
                 right -= 1
+
             return True
 
         for length in range(len(s), 0, -1):
             for start in range(len(s) - length + 1):
-                if check(start, start + length):
+                if checkPalindrome(start, start + length):
                     return s[start : start + length]
+
         return ""
