bdaffeaa9162b862c91b45b5aa9885e84e4308a1
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-21 04:37:22 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (30.21%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,19 +1,25 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
-        dp = {len(s): 1}
+        # Dynamic Programming Solution
+        n = len(s)
+        if not s or s[0] == "0":
+            return 0
+            
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
 
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == '0':
-                dp[i] = 0
+        for i in range(2, n + 1):
+            # One-digit check
+            if s[i - 1] != '0':
+                dp[i] += dp[i - 1]
 
-            else:
-                dp[i] = dp[i + 1]
-                
-            if (i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in "0123456")):
-                dp[i] += dp[i + 2]
-        return dp[0]
+            # Two-digit check
+            two_digit = int(s[i - 2:i])
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i - 2]
 
+        return dp[n]
 
         '''
         cache = {}
