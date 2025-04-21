d6888336bb4b3498fca2668384d4369f64a2f51b
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-21 04:29:04 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (98.32%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,5 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        
+        dp = {len(s): 1}
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+
+            else:
+                dp[i] = dp[i + 1]
+                
+            if (i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in "0123456")):
+                dp[i] += dp[i + 2]
+        return dp[0]
+
+
+        '''
         cache = {}
 
         def dfs(i):
@@ -14,10 +30,12 @@ class Solution:
             res = dfs(i + 1)
 
             # Decode two digits if it's between 10 and 26
-            if i + 1 < len(s) and 10 <= int(s[i:i+2]) <= 26:
+            if (i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in "0123456")):
                 res += dfs(i + 2)
 
             cache[i] = res
             return res
 
         return dfs(0)
+
+        '''
