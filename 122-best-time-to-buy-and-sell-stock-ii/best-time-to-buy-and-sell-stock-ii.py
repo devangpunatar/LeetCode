f0f7a9497a80e9709fd25f2a5d3f14af80e3aa2b
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-22 03:40:10 -0700

    Time: 9 ms (8.57%) | Memory: 18.7 MB (70.26%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,6 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
+        ahead = [0, 0]  # represents dp[i + 1]
+        curr = [0, 0]   # represents dp[i]
+
+        for i in range(n - 1, -1, -1):
+            for buy in [0, 1]:
+                if buy:
+                    curr[buy] = max(-prices[i] + ahead[0],
+                                    ahead[1])
+                else:
+                    curr[buy] = max(prices[i] + ahead[1],
+                                    ahead[0])
+            ahead = curr[:]
+
+        return ahead[1]
+
+
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
 
         # dp[i][buy] → max profit from day i with 'buy' permission
         dp = [[0] * 2 for _ in range(n + 1)]
@@ -14,3 +34,4 @@ class Solution:
                     dp[i][buy] = max(prices[i] + dp[i + 1][1], 0 + dp[i + 1][0])
 
         return dp[0][1]
+'''
