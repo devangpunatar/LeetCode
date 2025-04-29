79e3d4f092546db41bcabf212da5c66da68c9d2e
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-29 04:11:06 -0700

    Time: 274 ms (28.70%) | Memory: 28.9 MB (8.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
-        visited = set()
+        if not grid:
+            return 0
+        
         rows, cols = len(grid), len(grid[0])
+        visited = set()
+
         islands = 0
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        def bfs(r, c):
-            q = collections.deque()
-            q.append((r, c))
+
+        def dfs(r, c):
+            if (r < 0 or r > rows - 1 or c < 0 or c > cols - 1 or grid[r][c] == "0" or (r, c) in visited):
+                return 
+
             visited.add((r, c))
-            while q:
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if (r in range(rows) and c in range(cols) and
-                        (r, c) not in visited and grid[r][c] == '1'):
-                        q.append((r, c))
-                        visited.add((r, c))
-            
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    bfs(r, c)
+            dfs(r - 1, c)  # Up
+            dfs(r + 1, c)  # Down
+            dfs(r, c - 1)  # Left
+            dfs(r, c + 1)  # Right
+
+        for r in range (rows):
+            for c in range (cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    dfs(r, c)
                     islands += 1
 
-        return islands
-
+        return islands                
