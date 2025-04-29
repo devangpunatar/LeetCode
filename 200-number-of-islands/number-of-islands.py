def05a2e19e01d06a8e5392944dea146eaeee83b
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-29 03:46:57 -0700

    Time: 266 ms (36.14%) | Memory: 29 MB (5.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
+        if not grid:
+            return 0
+        
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        res = 0
+
+        islands = 0
+
         def dfs(r, c):
-            if (r < 0 or r == rows or c < 0 or c == cols or (r, c) in visited or grid[r][c] == "0"):
+            if (r < 0 or r > rows - 1 or c < 0 or c > cols - 1 or grid[r][c] == "0" or (r, c) in visited):
                 return False
+
             visited.add((r, c))
+            dfs(r - 1, c)  # Up
+            dfs(r + 1, c)  # Down
+            dfs(r, c - 1)  # Left
+            dfs(r, c + 1)  # Right
 
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+        for r in range (rows):
+            for c in range (cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    dfs(r, c)
+                    islands += 1
 
-            return True
-
-    
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r, c):
-                    res += 1
-        
-        return res
+        return islands                
