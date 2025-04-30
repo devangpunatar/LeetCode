3b43927303824fb1450980b9ad6d6fa1bfd428f5
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-04-30 04:36:47 -0700

    Time: 2 ms (90.96%) | Memory: 20.2 MB (6.64%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         preMap = {i: [] for i in range(numCourses)}
-
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
 
         visited = set()
+        visiting = set()
+
         def dfs(crs):
-            if crs in visited:
+            if crs in visiting: # cycle detected
                 return False
-            if preMap[crs] == []:
+            if crs in visited: # already processed
                 return True
-            visited.add(crs)
+            visiting.add(crs)
+
             for pre in preMap[crs]:
                 if not dfs(pre):
                     return False
-            visited.remove(crs)
-            preMap[crs] = []
+            visiting.remove(crs)
+            visited.add(crs)
             return True
-        
+
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
-
-    
-        
