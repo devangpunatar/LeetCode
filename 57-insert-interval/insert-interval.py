619ddf9ad8c075b181b9f9c627c76c84f6b0094b
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-05-03 06:50:20 -0700

    Time: 0 ms (100.00%) | Memory: 19.7 MB (40.67%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,25 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not intervals:
-            return [newInterval]
         res = []
-        start, end = newInterval[0], newInterval[1]
-        for i in range(len(intervals)):
-            if end < intervals[i][0] and res and start > res[-1][1]:
-                res.append([start, end])
-            elif end < intervals[i][0] and not res:
-                res.append([start, end])
-            if start > intervals[i][1]:
-                res.append(intervals[i])
-            elif end < intervals[i][0]:
-                res.append(intervals[i])
-            elif start <= intervals[i][1]:
-                start, end = min(intervals[i][0], start), max(intervals[i][1], end)
-            elif end >= intervals[i][0]:
-                start, end = min(intervals[i][0], start), max(intervals[i][1], end)
+        n = len(intervals)
+        i = 0
+        
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+        res.append(newInterval)
 
-        if not res:
-            res.append([start, end])
-        if start > res[-1][1]:
-            res.append([start, end])
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+    
         return res
