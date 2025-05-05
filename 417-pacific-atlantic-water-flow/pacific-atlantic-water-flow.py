c62740c5f62f5f93c094e4ec9a09b34f5d379653
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-05-04 23:58:07 -0700

    Time: 29 ms (76.86%) | Memory: 19.6 MB (17.00%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        atlantic, pacific = set(), set()
+
+        def dfs(r, c, visited, prevHeight):
+            if (r < 0 or r > rows - 1 or c < 0 or c > cols - 1 or
+                (r, c) in visited or heights[r][c] < prevHeight):
+                return 
+            visited.add((r, c))
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+        
+        for c in range(cols):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(rows - 1, c, atlantic, heights[rows - 1][c])
+
+        for r in range(rows):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, cols - 1, atlantic, heights[r][cols - 1])
+
+        return list(pacific & atlantic)
