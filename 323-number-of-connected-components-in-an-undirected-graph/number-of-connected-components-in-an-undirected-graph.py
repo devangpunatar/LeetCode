71af9d7432241ff0cfac3067d97adc55445a371c
devangpunatar <79669863+devangpunatar@users.noreply.github.com>
2025-05-06 12:40:02 -0700

    Time: 4 ms (73.03%) | Memory: 19.7 MB (24.58%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,33 +1,25 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        par = [i for i in range(n)]
-        rank = [1] * n
+        adj = {i: [] for i in range(n)}
 
-        def find(n1):
-            res = n1
-            while par[res] != res:
-                par[res] = par[par[res]]
-                res = par[res]
-            return res
-
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-
-            if p1 == p2:
-                return 0
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
             
-            if rank[p2] > rank[p1]:
-                par[p1] = p2
-                rank[p2] += rank[p1]
-            else:
-                par[p2] = p1
-                rank[p1] += rank[p2]
-            return 1
-        
-        res = n
-        for n1, n2 in edges:
-            res -= union(n1, n2)
-            
+        visited = set()
+        res = 0
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+
+            for nei in adj[node]:
+                if nei not in visited:
+                    dfs(nei)
+            return
+
+        for node in range(n):
+            if node not in visited:
+                dfs(node)
+                res += 1 
         return res
-
-            
